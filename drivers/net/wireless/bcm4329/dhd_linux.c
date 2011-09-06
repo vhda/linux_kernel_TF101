334271907854b80eea756a451924e75467d008b9
Vitor Antunes <vitor.hda@gmail.com>
2011-09-06 01:31:27 +0100

    Correct bcm4329 to be detected by network-manager
    Patch obtained from:
    http://htc-linux.org/wiki/index.php?title=LeoWifi#BCM4329_interface_detection_problem

--- drivers/net/wireless/bcm4329/dhd_linux.c
+++ drivers/net/wireless/bcm4329/dhd_linux.c
@@ -2028,7 +2028,7 @@ dhd_del_if(dhd_info_t *dhd, int ifidx)
 
 
 dhd_pub_t *
-dhd_attach(osl_t *osh, struct dhd_bus *bus, uint bus_hdrlen)
+dhd_attach(osl_t *osh, struct dhd_bus *bus, uint bus_hdrlen, struct device *parent)
 {
 	dhd_info_t *dhd = NULL;
 	struct net_device *net;
@@ -2045,6 +2045,7 @@ dhd_attach(osl_t *osh, struct dhd_bus *bus, uint bus_hdrlen)
 		DHD_ERROR(("%s: OOM - alloc_etherdev\n", __FUNCTION__));
 		goto fail;
 	}
+	SET_NETDEV_DEV(net, parent);
 
 	/* Allocate primary dhd_info */
 	if (!(dhd = MALLOC(osh, sizeof(dhd_info_t)))) {
